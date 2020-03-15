1a6cfa54099dd39c0ca3b830ad94d18f0976a70f
Lucas Souza <lucassouza690@gmail.com>
2020-03-14 21:19:45 -0300

    First Commit

---
+++ Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -0,0 +1,48 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoTestes
+    {
+        [Fact]
+        public void LeilaoComVariosLances()
+        {
+            var leilao = new Leilao("Van Gough");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 990);
+
+            leilao.TerminaPregao();
+
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+
+        }
+
+        [Fact]
+        public void LeilaoComApenasUmLance()
+        {
+            var leilao = new Leilao("Van Gough");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            leilao.TerminaPregao();
+
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
---
+++ Alura.LeliaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+
+namespace Alura.LeliaoOnline.ConsoleApp
+{
+    class Program
+    {
+
+        private static void Verifica(double esperado, double obtido)
+        {
+            if(esperado == obtido)
+            {
+                Console.WriteLine("TESTE OK");
+            }
+            else
+            {
+                Console.WriteLine("TESTE FALHOU");
+            }
+        }
+
+        private static void LeilaoComVariosLances()
+        {
+            var leilao = new Leilao("Van Gough");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 990);
+
+            leilao.TerminaPregao();
+
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+
+        }
+
+        private static void LeilaoComApenasUmLance()
+        {
+            var leilao = new Leilao("Van Gough");
+            var fulano = new Interessada("Fulano", leilao);
+            
+            leilao.RecebeLance(fulano, 800);
+           
+            leilao.TerminaPregao();
+
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        static void Main(string[] args)
+        {
+            LeilaoComVariosLances();
+            LeilaoComApenasUmLance();
+        }
+
+        
+    }
+}
